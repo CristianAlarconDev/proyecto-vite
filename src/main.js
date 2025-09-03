5e313e591698ebdc818ee03d51012dda3878e7b0
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-03 13:07:28 -0300

    Se agrego refactor del main.js, separar luego en solo un archivo header.js para mejor manejo. Menu desplegable funcionando. Mejorar los estilos de nav-links open.

--- src/main.js
+++ src/main.js
@@ -3,19 +3,35 @@ import './header.css';
 
 const menuBtn = document.querySelector(".menu-btn");
 const navLinks = document.querySelector(".nav-links");
+const iconNav=menuBtn.querySelector("i");
 
-menuBtn.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
+/*Despliegue menu colapsable */
+
+menuBtn.addEventListener("click", () => 
+  desplegarNav(navLinks));
+
+
+function desplegarNav(navADesplegar){
+  navADesplegar.classList.toggle("open");
   const icon = menuBtn.querySelector("i");
   icon.classList.toggle("fa-bars");
   icon.classList.toggle("fa-xmark");
-});
+}
 
-window.addEventListener("resize", () => {
-  if (window.innerWidth > 720) {
-    navLinks.classList.remove("open");
-    const icon = menuBtn.querySelector("i");
-    icon.classList.add("fa-bars");
-    icon.classList.remove("fa-xmark");
+/* Reinicio de imagen en menu hamburguesa y de condiciones en css dentro clase open.*/
+
+window.addEventListener("resize", () => 
+  redimensionarHeader(navLinks, "open"));
+
+
+function redimensionarHeader(listaLinks, quitarSelector){
+  if(window.innerWidth>720){
+    listaLinks.classList.remove(quitarSelector);
+    cambiarIcono(iconNav,"fa-bars","fa-xmark");
   }
-});
+}
+
+function cambiarIcono(icono, Iagregar, Ieliminar){
+  icono.classList.add(Iagregar);
+  icono.classList.remove(Ieliminar);
+}
