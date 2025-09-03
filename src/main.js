56702043ac98400abcc87bc5b4ebfe23c685d0cb
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-03 06:06:06 -0300

    refactor de menu

--- src/main.js
+++ src/main.js
@@ -2,22 +2,18 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import './header.css';
 
 const menuBtn = document.querySelector(".menu-btn");
-const navMobile = document.querySelector(".nav-mobile");
+const navLinks = document.querySelector(".nav-links");
 
 menuBtn.addEventListener("click", () => {
-  navMobile.classList.toggle("open");
-
-  // Cambiar icono
+  navLinks.classList.toggle("open");
   const icon = menuBtn.querySelector("i");
   icon.classList.toggle("fa-bars");
   icon.classList.toggle("fa-xmark");
 });
 
-// Escuchar resize
 window.addEventListener("resize", () => {
   if (window.innerWidth > 720) {
-    // cerrar menú mobile y resetear icono
-    navMobile.classList.remove("open");
+    navLinks.classList.remove("open");
     const icon = menuBtn.querySelector("i");
     icon.classList.add("fa-bars");
     icon.classList.remove("fa-xmark");
