10a6896401f3c7a1147504ac6ddda755b22fe5f1
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-03 22:10:11 -0300

    Se reestructuro el proyecto, falta aun trabajar en el hero

--- src/js/header.js
+++ src/js/header.js
@@ -1,6 +1,3 @@
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import './header.css';
-import './main.css';
 
 const menuBtn = document.querySelector(".menu-btn");
 const navLinks = document.querySelector(".nav-links");
@@ -9,30 +6,30 @@ const iconNav=menuBtn.querySelector("i");
 /*Despliegue menu colapsable */
 
 menuBtn.addEventListener("click", () => 
-  desplegarNav(navLinks));
+    desplegarNav(navLinks));
 
 
 function desplegarNav(navADesplegar){
-  navADesplegar.classList.toggle("open");
-  const icon = menuBtn.querySelector("i");
-  icon.classList.toggle("fa-bars");
-  icon.classList.toggle("fa-xmark");
+    navADesplegar.classList.toggle("open");
+    const icon = menuBtn.querySelector("i");
+    icon.classList.toggle("fa-bars");
+    icon.classList.toggle("fa-xmark");
 }
 
 /* Reinicio de imagen en menu hamburguesa y de condiciones en css dentro clase open.*/
 
 window.addEventListener("resize", () => 
-  redimensionarHeader(navLinks, "open"));
+    redimensionarHeader(navLinks, "open"));
 
 
 function redimensionarHeader(listaLinks, quitarSelector){
-  if(window.innerWidth>720){
+    if(window.innerWidth>720){
     listaLinks.classList.remove(quitarSelector);
     cambiarIcono(iconNav,"fa-bars","fa-xmark");
-  }
+    }
 }
 
 function cambiarIcono(icono, Iagregar, Ieliminar){
-  icono.classList.add(Iagregar);
-  icono.classList.remove(Ieliminar);
+    icono.classList.add(Iagregar);
+    icono.classList.remove(Ieliminar);
 }
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './header.js';
+import './hero.js';
+import '../css/header.css';
+import '../css/main.css';
