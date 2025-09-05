25ebb5cc61bbac6fa8b10a4a1e59b151f331b0a9
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-05 13:45:24 -0300

    se refactoriza codigo en hero.js; los slides se cargan ok y se leen luego en el js desde html para darle mas condiciones; se agrega clase carroussel sin terminar, falta poner metodo privado para renderizar slide e inyectar nuevamente al html, hay que mover las funciones fuera de la clase o adaptarlas para eso

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,57 +1,87 @@
 /* Repasar concepto de promesas y async*/
 async function cargarData() {
-    const respuesta = await fetch('/data/hero.json'); 
-    const dataHero = await respuesta.json();
-    
-    const templateHTML =document.querySelector('#slide-template');
-    dataHero.forEach(data => { 
-        slideDesdeTemplate(data, templateHTML);
-        
-    });
-/*
-    dataHero.forEach(data => 
-        {const section =crearSlide(data);
-            console.log(section.outerHTML);
-        contenedorHTML.appendChild(section);
-        });*/
-    return true;
-    }
-
-function slideDesdeTemplate(dataJSON, template){
-    const contenedorHTML=document.querySelector("#hero");
-    const plantilla = template.content.cloneNode(true);
-    const seccion= plantilla.querySelector('.slide');
-    seccion.style.backgroundImage=`url(${dataJSON.img})`;
-    seccion.querySelector('h2').textContent = dataJSON.titulo;
-    seccion.querySelector('p').textContent = dataJSON.descripcion;
-    contenedorHTML.appendChild(plantilla);
+  const respuesta = await fetch("/data/hero.json");
+  return await respuesta.json();
 }
 
-function crearSlide(dataJSON){
+function renderizarSlide(dataJSON, template) {
+  const plantilla = template.content.cloneNode(true);
+  const seccion = plantilla.querySelector(".slide");
 
-    const section = document.createElement('section');
-    section.classList.add('slide');
-    //const imagen = document.createElement('img');
-    //imagen.src=dataJSON.img;
-    section.style.backgroundImage = `url(${dataJSON.img})`;
-    const titulo=document.createElement('h2');
-    titulo.textContent=dataJSON.titulo;
-    const parrafo= document.createElement('p');
-    parrafo.textContent=dataJSON.descripcion;
-    section.append(titulo, parrafo);
-    //section.append(imagen, titulo, parrafo);
-    return section;
+  seccion.style.backgroundImage = `url(${dataJSON.img})`;
+  seccion.querySelector("h2").textContent = dataJSON.titulo;
+  seccion.querySelector("p").textContent = dataJSON.descripcion;
+
+  //console.log(seccion.outerHTML);
+  return plantilla;
+}
+function renderHero(slidesData) {
+  const contenedorHTML = document.querySelector("#hero");
+  const templateHTML = document.querySelector("#slide-template");
+  slidesData.forEach((data) => {
+    const slide = renderizarSlide(data, templateHTML);
+
+    contenedorHTML.appendChild(slide);
+  });
 }
 
-
-function obtenerSlides(){
-    const slides = document.querySelectorAll('#hero .slide');
-    //console.log(slides);
-    return slides;
+function obtenerSlides() {
+  const slides = document.querySelectorAll("#hero .slide");
+  return slides;
 }
 async function iniciarHero() {
-    await cargarData();        
-    obtenerSlides();           
-}
+  const dataHero = await cargarData();
+  renderHero(dataHero);
 
-iniciarHero();
+  //const slides=obtenerSlides();
+  //console.log(slides);
+}
+/*
+function mostrarSlide(indiceSlide) {
+  const slides = obtenerSlides();
+  slides.forEach((slide, indice) => {
+    if (indice === indiceSlide) {
+      slides[indice].style.display = "flex";
+    } else {
+      slides[indice].style.display = "none";
+    }
+  });
+  console.log(slides[indiceSlide]);
+}
+*/
+
+async function iniciarCarroussel() {
+  //const indice = 1;
+  await iniciarHero();
+  //mostrarSlide(indice);
+}
+iniciarCarroussel();
+
+class Carroussel {
+  constructor(selector) {
+    this.contenedor = document.querySelector(selector);
+    this.slides = [];
+    this.indiceActual = 0;
+  }
+
+  mostrarSlide(indiceSlide) {
+    slides.forEach((slide, indice) => {
+      if (indice === indiceSlide) {
+        slides[indice].style.display = "flex";
+      } else {
+        slides[indice].style.display = "none";
+      }
+    });
+    console.log(slides[indiceSlide]);
+  }
+  siguienteSlide() {
+    this.indiceActual = (this.indiceActual + 1) % this.slides.length;
+    this.mostrarSlide(this.indiceActual);
+  }
+
+  anteriorSlide() {
+    this.indiceActual = (this.indiceActual - 1 + this.slides.length) % this.slides.length;
+    this.mostrarSlide(this.indiceActual);
+  }
+
+}
