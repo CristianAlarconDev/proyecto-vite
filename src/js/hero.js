9450d0affe5cdd27bf20fd88b9eca3ea1fb8dad3
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-05 05:21:49 -0300

    se hizo refactor a hero.js, se cargaron slides dinamicos usando template html y se agrego estilos basicos en hero.css; falta refactorizar mas hero.js y terminar la funcion iniciar carrousel ahi

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,28 +1,57 @@
 /* Repasar concepto de promesas y async*/
-async function cargarHero() {
+async function cargarData() {
     const respuesta = await fetch('/data/hero.json'); 
     const dataHero = await respuesta.json();
-    const contenedorHTML=document.querySelector("#hero");
+    
+    const templateHTML =document.querySelector('#slide-template');
+    dataHero.forEach(data => { 
+        slideDesdeTemplate(data, templateHTML);
+        
+    });
+/*
     dataHero.forEach(data => 
-        {const section =crearSlide(data)
+        {const section =crearSlide(data);
             console.log(section.outerHTML);
         contenedorHTML.appendChild(section);
-        });
+        });*/
+    return true;
+    }
+
+function slideDesdeTemplate(dataJSON, template){
+    const contenedorHTML=document.querySelector("#hero");
+    const plantilla = template.content.cloneNode(true);
+    const seccion= plantilla.querySelector('.slide');
+    seccion.style.backgroundImage=`url(${dataJSON.img})`;
+    seccion.querySelector('h2').textContent = dataJSON.titulo;
+    seccion.querySelector('p').textContent = dataJSON.descripcion;
+    contenedorHTML.appendChild(plantilla);
 }
 
 function crearSlide(dataJSON){
+
     const section = document.createElement('section');
     section.classList.add('slide');
-    const imagen = document.createElement('img');
-    imagen.src=dataJSON.img;
+    //const imagen = document.createElement('img');
+    //imagen.src=dataJSON.img;
+    section.style.backgroundImage = `url(${dataJSON.img})`;
     const titulo=document.createElement('h2');
     titulo.textContent=dataJSON.titulo;
     const parrafo= document.createElement('p');
     parrafo.textContent=dataJSON.descripcion;
-
-    section.append(imagen, titulo, parrafo);
+    section.append(titulo, parrafo);
+    //section.append(imagen, titulo, parrafo);
     return section;
 }
 
-cargarHero();
 
+function obtenerSlides(){
+    const slides = document.querySelectorAll('#hero .slide');
+    //console.log(slides);
+    return slides;
+}
+async function iniciarHero() {
+    await cargarData();        
+    obtenerSlides();           
+}
+
+iniciarHero();
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,18 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import './header.js';
-import './hero.js';
 import '../css/header.css';
 import '../css/hero.css';
 import '../css/main.css';
+import './header.js';
+import './hero.js';
+
+
+/*otra forma de usar una funcion que necesita al dom solo cuando esta cargado, al estar en modulos por vite
+ no deberia hacer falta; leer mas de es6 modules luego*/
+ 
+/*document.addEventListener("DOMContentLoaded", ()=> 
+{
+    cargarHero();
+}
+);
+*/
+
